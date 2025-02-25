d2b1f1e595f775bd124d104b944bbcccd1d3a291
Adam Bonsu <adam@bonsu.io>
2025-02-25 09:17:19 +0000

    display port number used on startup

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ require("dotenv").config();
 // Get the port from the environment variable or use 3000 as default
 const port = process.env.PORT || 3000;
 
-console.log("DEBUG: PORT = " + port);
 const app = require("./app");
 app.listen(port, () => {
-  console.log("Server is now running");
+  console.log("Server is now running on port " + port);
 });

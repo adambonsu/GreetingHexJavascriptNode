58a0397596e8f516f900aa7ed228685e8ba4d75e
Adam Bonsu <adam@bonsu.io>
2025-02-25 08:53:44 +0000

    Get node to serve app based on PORT environment variable

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
+require("dotenv").config();
+
+// Get the port from the environment variable or use 3000 as default
+const port = process.env.PORT || 3000;
+
+console.log("DEBUG: PORT = " + port);
 const app = require("./app");
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("Server is now running");
 });

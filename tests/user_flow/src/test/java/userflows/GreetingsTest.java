72d3107dfb4feefb18821bc8f4de99c2bc6d1504
Adam Bonsu <adam@bonsu.io>
2025-02-26 17:05:30 +0000

    give unique ids to all static greetings

--- tests/user_flow/src/test/java/userflows/GreetingsTest.java
+++ tests/user_flow/src/test/java/userflows/GreetingsTest.java
@@ -1,6 +1,7 @@
 package userflows;
 
 import io.restassured.RestAssured;
+
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
@@ -11,6 +12,12 @@ import org.junit.jupiter.api.DisplayName;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class GreetingsTest {
 	
@@ -19,9 +26,9 @@ public class GreetingsTest {
 		RestAssured.baseURI = "http://localhost:3000";
 	}
 	
-	@DisplayName("Get all greetings")
+	@DisplayName("Get all greetings returns list of Greetings")
 	@Test
-	void testGetAllGreetings() {
+	void testGetAllGreetingsReturnsListOfGreetings() {
 		Response response = get("/greetings");
 		response.prettyPrint();
 		given()
@@ -60,6 +67,12 @@ public class GreetingsTest {
 
 			))
 			.body("[0]", anyOf(
+					hasKey("timeOfDay"),
+					not(hasKey("timeOfDay")
+				)
+
+			))
+			.body("[0]", anyOf(
 					hasKey("createdAt"),
 					not(hasKey("createdAt")
 				)
@@ -74,6 +87,19 @@ public class GreetingsTest {
 			
 			
 		
+	}
+	
+	@DisplayName("Get all greetings returns list of Greetings with unique Ids")
+	@Test
+	void testGetAllGreetingsReturnsListOfGreetingsWithUniqueIDs() {
+		List<Integer> ids = given()
+			.contentType(ContentType.JSON)
+		.when()
+			.get("/greetings")
+		.then()
+			.extract().jsonPath().getList("id", Integer.class);
+		Set<Object> uniqueIds = ids.stream().collect(Collectors.toSet());
+        assertEquals(ids.size(), uniqueIds.size(), "All ids should be unique");
 	}
 
 }

d35ca8920f26f1347c654894a8716b2304721ac6
Adam Bonsu <adam@bonsu.io>
2025-02-28 19:14:09 +0000

    integration tests updated

--- tests/integration/greeting.controller.int.test.js
+++ tests/integration/greeting.controller.int.test.js
@@ -1,14 +1,14 @@
 const request = require("supertest");
 const app = require("../../app");
-const endpointUrl = "/greeting";
+const endpointUrl = "/greetings";
 
 describe("Greeting endpoint", () => {
-  test("GET /greeting returns 200 with list of greetings", async () => {
+  test("GET /greetings returns 200 with list of greetings", async () => {
     const response = await request(app).get(endpointUrl);
     expect(response.statusCode).toBe(200);
     expect(typeof response.body).toBe("object");
     response.body.forEach((greeting) => {
-      expect(greeting).toHaveProperty("greeting");
+      expect(greeting).toHaveProperty("message");
     });
     expect(response.body.length).toBeGreaterThan(0);
   });
